cff817161e618d6479e0a309d0f203aa01bf5f9d
HyperScripts <111307220+HyperScripts@users.noreply.github.com>
2022-08-15 15:33:54 -0300

    Update and rename README.md to README.lua

---
+++ README.lua
@@ -0,0 +1,9 @@
+Exports do sistema vip, caso não entenda sobre o assunto, abra um ticket em nossa loja para mais detalhes.
+
+
+exports['[HS]SistemaVIP']:givePlayerVip(player, '1')-- elemento player, tipo de vip: ('1', '2', '3', '4')
+exports['[HS]SistemaVIP']:givePlayerPayment(player, 100)-- elemento player, value
+exports['[HS]SistemaVIP']:generateKey(10, 100)-- length key, value
+exports['[HS]SistemaVIP']:deleteFromKey('G5ZSKADNFF')-- key string
+
+💙 Para mais detalhes sobre: discord.gg/hyperscripts
--- README.md
+++
@@ -1,2 +0,0 @@
-# Exports-System-Vip
-Todas as exports do sistema vip
