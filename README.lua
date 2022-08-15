ea5d86e0715db59e5c155f7cf1e343fd95e71871
HyperScripts <111307220+HyperScripts@users.noreply.github.com>
2022-08-15 16:10:58 -0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -1,9 +1,1 @@
-Exports do sistema vip, caso não entenda sobre o assunto, abra um ticket em nossa loja para mais detalhes.
-
-
-exports['[HS]SistemaVIP']:givePlayerVip(player, '1')-- elemento player, tipo de vip: ('1', '2', '3', '4')
-exports['[HS]SistemaVIP']:givePlayerPayment(player, 100)-- elemento player, value
-exports['[HS]SistemaVIP']:generateKey(10, 100)-- length key, value
-exports['[HS]SistemaVIP']:deleteFromKey('G5ZSKADNFF')-- key string
-
-💙 Para mais detalhes sobre: discord.gg/hyperscripts
+<h1 Documentação disponível do sistema vip </h1>
